fe59d6350e8ab88df9ae0ea504067b035289d004
原德 邵 <rayroy0322@gmail.com>
2020-10-29 11:50:06 +0800

    123

---
+++ WebApplication5/Scripts/mapmodile.js
@@ -0,0 +1,38 @@
+﻿//現在位子
+var maplist = [
+    {
+        'map1': "白玉樓",
+        'map2': "宅邸",
+        'mapid':"a_a0101",
+        "x": 1,
+        "y": 1,
+    }
+] 
+
+var map = [
+    {
+        'map1': "白玉樓",
+        'map2': "宅邸",
+        'meet': [
+            {
+                'characterID': 'b_b0101',
+                'Probability':5
+            },
+            {
+                'characterID': 'b_b0102',
+                'Probability': 5
+            },
+        ],
+        'collection': [
+            {
+                'collectionID': 'c_b0101',
+                'Probability': 5
+            },
+            {
+                'collectionID': 'c_b0102',
+                'Probability': 5
+            },
+        ],    
+    },
+
+] 
